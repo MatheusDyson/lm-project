4fe2be6f1df53ae3222c700cece345bb05bcb8fd
Luiz Antunes <luizrantunes@outlook.com>
2023-02-05 18:45:43 -0300

    final update today

--- Guess-the-Pokemon/index.js
+++ Guess-the-Pokemon/index.js
@@ -7,15 +7,15 @@ function pokedex (name, type, generation) {
     return pkmn
 }
 
-const pkmn1 = pokedex('Bulbasaur', 'Grama', 'Geração 1')
-const pkmn2 = pokedex('Charmander', 'Fogo', 'Geração 1')
-const pkmn3 = pokedex('Squirtle', 'Água', 'Geração 1')
-const pkmn4 = pokedex('Chikorita', 'Grama', 'Geração 2')
-const pkmn5 = pokedex('Cyndaquil', 'Fogo', 'Geração 2')
-const pkmn6 = pokedex('Totodile', 'Água', 'Geração 2')
-const pkmn7 = pokedex('Treecko', 'Grama', 'Geração 3')
-const pkmn8 = pokedex('Torchic', 'Fogo', 'Geração 3')
-const pkmn9 = pokedex('Mudkip', 'Água', 'Geração 3')
+const pkmn1 = pokedex('Bulbasaur', 'Grama', 1)
+const pkmn2 = pokedex('Charmander', 'Fogo', 1)
+const pkmn3 = pokedex('Squirtle', 'Água', 1)
+const pkmn4 = pokedex('Chikorita', 'Grama', 2)
+const pkmn5 = pokedex('Cyndaquil', 'Fogo', 2)
+const pkmn6 = pokedex('Totodile', 'Água', 2)
+const pkmn7 = pokedex('Treecko', 'Grama', 3)
+const pkmn8 = pokedex('Torchic', 'Fogo', 3)
+const pkmn9 = pokedex('Mudkip', 'Água', 3)
 
 
 const pokemonRandom = [pkmn1, pkmn2, pkmn3,
@@ -34,13 +34,27 @@ console.log(correctName)
 console.log(correctType)
 console.log(correctGeneration)
 
+let attempts = 0
+let maxguesses = 3
+
 function comparePokemon() {
     const userPokemon = document.getElementById("userPokemon").value
 
-    if (userPokemon.toLowerCase() === correctName.toLowerCase()) {
-        document.getElementById("answer").innerHTML = "Boa! Era o " + correctName + "."
-    } else {
-        document.getElementById("userPokemon").value = ""
-        document.getElementById("tipType").innerHTML = "Dica 1: O pokémon é do tipo " + correctType.toLowerCase() + "."
+    if (attempts < maxguesses) {
+        if (userPokemon.toLowerCase() === correctName.toLowerCase()) {
+            document.getElementById("answer").innerHTML = "Boa! Era o " + correctName + "."
+            document.getElementById("userPokemon").setAttribute("Readonly", "Readonly")
+        } else if (userPokemon.toLowerCase() !== correctName.toLowerCase() && attempts === 1) {
+            document.getElementById("userPokemon").value = ""
+            document.getElementById("tipGeneration").innerHTML = "Dica 2: O pokémon é da " + correctGeneration + "ª geração."
+            attempts++
+        } else
+            document.getElementById("userPokemon").value = ""
+            document.getElementById("tipType").innerHTML = "Dica 1: O pokémon é do tipo " + correctType.toLowerCase() + "."
+            attempts++
+    }
+    else {
+        document.getElementById("answer").innerHTML = "Fim de jogo!" + "</br>" + "O pokémon escondido era o " + correctName + "."
+        document.getElementById("userPokemon").setAttribute("Readonly", "Readonly")
     }
 }
